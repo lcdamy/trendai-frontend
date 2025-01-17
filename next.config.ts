fec6d52115fff09bd2db4f0a3dd055d62e6356ee
lcdamy <zudanga@gmail.com>
2025-01-17 20:05:15 +0200

    adding backend in allowed domain

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["localhost", "plus.unsplash.com"],
+    domains: ["localhost", "plus.unsplash.com","trendai-backend-production-f6c2.up.railway.app"],
   },
 };
 
